0ff48ac18cb54698cc497474e1a949c4275fd8bb
uzwhite444 <gocsgo398@gmail.com>
2023-10-20 18:05:59 +0500

    asdsaD

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,13 +1,29 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import './Header.scss'
-import { Link } from 'react-router-dom'
 import './Header.scss'
 import Logo from "../../img/Logo.png"
 function Header() {
+  
+  const [vWidth, setVWidth] = useState(window.innerWidth);
+  window.addEventListener("resize", () => {
+    setVWidth(window.innerWidth);
+  });
+  useEffect(() => {
+    const handleScroll = () => {
+      const nav = document.querySelector(".Header");
+      nav.classList.toggle("sticky", window.scrollY > 0);
+    };
+
+    window.addEventListener("scroll", handleScroll);
+
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
   return (
     <div className='Header'>
       <div className="container">
-        <div className="Header">
+        <div className="Header ">
           <div className="logo__container">
             <img className='Logo' src={Logo} alt="" />
             <h4 className='Logo__text'>Muhammadabdulloh</h4>
